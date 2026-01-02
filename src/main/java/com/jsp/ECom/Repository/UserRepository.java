7894a0a69f4022def6c44138a79a7741d2e6799e
adii-13 <hsadithyachakravarthy@gmail.com>
2026-01-02 19:21:34 +0530

    Second Commit

---
+++ src/main/java/com/jsp/ECom/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jsp.ECom.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jsp.ECom.Entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	boolean existsByEmail(String adminEmail);
+	
+}
